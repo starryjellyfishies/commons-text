a92f1e3bd0ac7c4abf36593a8b96196381618383
sabdoun <sabdoun@bgsu.edu>
2025-04-27 19:27:22 -0400

    Test 6 - JaccardDistance boundary test

---
+++ src/test/java/org/apache/commons/text/similarity/AnotherJaccardDistTest.java
@@ -0,0 +1,68 @@
+/*
+* Licensed to the Apache Software Foundation (ASF) under one or more
+* contributor license agreements.  See the NOTICE file distributed with
+* this work for additional information regarding copyright ownership.
+* The ASF licenses this file to You under the Apache License, Version 2.0
+* (the "License"); you may not use this file except in compliance with
+* the License.  You may obtain a copy of the License at
+*
+*      http://www.apache.org/licenses/LICENSE-2.0
+*
+* Unless required by applicable law or agreed to in writing, software
+* distributed under the License is distributed on an "AS IS" BASIS,
+* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+* See the License for the specific language governing permissions and
+* limitations under the License.
+*/
+
+package org.apache.commons.text.similarity;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Test;
+
+public class AnotherJaccardDistTest {
+  
+  private final JaccardDistance jacDist = new JaccardDistance();
+
+  //Test 1 - Null inputs should throw exceptions
+  @Test
+  public void testNullInput(){
+    assertThrows(IllegalArgumentException.class, () -> {
+      jacDist.apply(null, "test");
+    });
+    assertThrows(IllegalArgumentException.class, () -> {
+      jacDist.apply("test", null);
+    });
+  }
+  
+  //Test 2 - Empty strings should return 0
+  @Test
+  public void testEmptyStrings(){
+    assertEquals(0.0, jacDist.apply("", ""));
+  }
+
+  //Test 3 - Test regular identical values
+  @Test
+  public void identValues(){
+    assertEquals(0.0, jacDist.apply("abcd", "abcd"));
+    assertEquals(0.0, jacDist.apply("testing", "testing"));
+
+  }
+
+  //Test 4 - test regular different values
+  @Test
+  public void testRegularValues(){
+    assertEquals(1.0, jacDist.apply("testing", "qwry"));
+    assertEquals(1.0, jacDist.apply("abcd", "efgh"));
+    assertEquals(1.0, jacDist.apply("hamburger", "pizza"), 0.1);
+  }
+
+  //Test 5 - test regular similar values
+  @Test
+  public void testSimilarValues(){
+    assertEquals(0.0, jacDist.apply("abc", "acb"));
+    assertEquals(0.5, jacDist.apply("test", "testing"));
+  }
+
+}
